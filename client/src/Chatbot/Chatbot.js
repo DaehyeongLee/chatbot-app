8d86984eda2c408cf73c55602578cf410dce9706
daelee <apm5018@naver.com>
2021-05-28 22:38:01 +0900

    Text Query Function for Front-end

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,18 +1,61 @@
 import React from 'react'
+import Axios from 'axios'
 
 function Chatbot() {
+
+    const textQuery = async(text) => {
+
+        //let conversationList = [] //conversations
+    
+        //유저가 보낸 Text로 변수 초기화
+        let conversation = {
+            who: 'user',
+            content: {
+                text: {
+                    text: text
+                }
+            }
+        }
+        //conversationList.push(conversation) //유저의 입력을 대화 기록에 push
+
+        const textQueryVariable= {
+            text
+        }
+
+        //Text Query router로 request를 보내기
+        try {
+            const response = await Axios.post('/api/dialogflow/textQuery', textQueryVariable)
+            const content = response.data.fulfillmentMessages[0] //Chatbot의 response
+            conversation = {
+                who: 'bot',
+                content: content
+            }
+            console.log(conversation)
+            //conversationList.push(conversation) //chatbot의 답장을 대화 기록에 push
+        } catch (error) {
+            conversation = {
+                who: 'user',
+                content: {
+                    text: {
+                        text: "Error occured"
+                    }
+                }
+            }
+            //conversationList.push(conversation)
+        }
+       
+
+    }
     
     const keyPressHandler = (e) => {
+
         //Enter key press (keyboard)
         if(e.key === "Enter") {
             //아무것도 입력되지 않았을 경우
             if(!e.target.value) {
                 return alert('Need to type something')
-            }
-
-            //Text Query router로 request를 보내기
-
-
+            }           
+            textQuery(e.target.value);
 
             e.target.value = ""; //입력했던 값 초기화
         }
